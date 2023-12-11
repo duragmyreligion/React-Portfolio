1c04834f92e3c76e0000fb229a577c2d483771b8
DuragMyReligion <samuelkininmonth@hotmail.com>
2023-12-11 16:39:47 +0800

    deployment fix

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -7,7 +7,7 @@ const Project = ({ title, description, images, links }) => {
       {/* MDBReact UI Kit Carousel */}
       <MDBCarousel showIndicators>
         {images.map((image, index) => (
-          <MDBCarouselItem key={index} itemId={index + 1} interval={4000}>
+          <MDBCarouselItem key={index} itemId={index + 1} interval={3000}>
             <div className='carousel-styling'>
             <a href={links[index]} target="_blank" rel="noopener noreferrer">
               <img src={image} className="d-block w-100" alt={`Slide ${index + 1}`} />
